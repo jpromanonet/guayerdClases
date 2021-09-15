7055968a7f585e36455881c20199fd8f6edebe83
jpromanonet <programming@jpromano.net>
2021-09-15 10:53:49 -0300

    Intro a JS - Variables

---
+++ Variables/app.js
@@ -0,0 +1,9 @@
+// Variables
+
+// La vieja forma = scope global
+var numerosEnteros;
+
+// Las nuevas formas de declarar variables (ES6)
+
+let miSegundaVariable; // scope local
+const otraFormaDeVariable; // scope mixto (global y local segun donde se declare)
