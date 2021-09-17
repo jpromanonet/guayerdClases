ef05bf4ed26ea250d73e1b635654f5d0fc7cbcfe
jpromanonet <programming@jpromano.net>
2021-09-17 11:51:19 -0300

    Condicionales

---
+++ condicionales/app.js
@@ -0,0 +1,22 @@
+let edad;
+
+edad = prompt('¿Que edad tenes?');
+
+// ESTO ES UNA CONDICION
+if(parseInt(edad) >= 18){
+    alert("Sos mayor de edad");
+    let franjaEtaria = prompt("¿Sos mayor de 30?");
+    if(parseInt(franjaEtaria) >= 30){
+        alert("Sos un millenial");
+    } else {
+        alert("Sos generacion Z");
+    }
+} else {
+    alert("No sos mayor de edad");
+    let quienSos = prompt("¿Sos Alexis?")
+    if(quienSos == "Si"){
+        alert("Anda a mirar Backyarddigan");
+    } else {
+        alert("Que bajon, yo esperaba a Alexis ¬¬");
+    }
+}
