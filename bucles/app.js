7ac84387e3513331a188d7dc9c874398568a76c7
jpromanonet <programming@jpromano.net>
2021-09-22 11:47:56 -0300

    Bucles FOR y WHILE

---
+++ bucles/app.js
@@ -0,0 +1,20 @@
+// Bucle FOR
+/*alert("Esto el alert 1");
+alert("Esto el alert 2");
+alert("Esto el alert 3");
+alert("Esto el alert 4");
+alert("Esto el alert 5");*/
+
+// Hacemos lo mismo pero usando un FOR
+var i = 5;
+
+for(var i = 1; i < 6; i++) {
+    alert("Esto es el alert " + i);
+}
+
+/*
+
+variable++ = el simbolo ++ a la derecha, en JS suma un entero a un valor numerico(1)
+
+*/
+
