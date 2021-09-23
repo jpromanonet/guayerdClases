954b89bdaf165fa18ca491fee05a950196258a71
jpromanonet <programming@jpromano.net>
2021-09-23 10:44:02 -0300

    Bucles FOR y WHILE

--- condicionales/app.js
+++ condicionales/app.js
@@ -1,3 +1,4 @@
+/*
 let edad;
 let siFuma;
 
@@ -5,6 +6,7 @@ edad = prompt('¿Que edad tenes?');
 siFuma = confirm('¿Sos fumador?');
 
 // ESTO ES UNA CONDICION
+
 if(parseInt(edad) >= 18 && siFuma === true ){
     alert("Sos mayor de edad");
     alert("Fumar es perjudicial para la salud")
@@ -19,6 +21,43 @@ if(parseInt(edad) >= 18 && siFuma === true ){
         } else {
             alert("Que bajon, yo esperaba a Alexis ¬¬");
         }
+}
+
+
+if(edad !== 27){
+    alert("No perteneces al club de los 27");
+}
+*/
+
+// SWITCH
+var day;
+day = prompt("¿Que dia de la semana buscas?");
+
+switch(day){
+    case "1":
+        alert("Es Domingo!");
+        break;
+    case "2":
+        alert("Es Lunes!");
+        break;
+    case "3":
+        alert("Es Martes");
+        break;
+    case "4":
+        alert("Es Miercoles =)");
+        break;
+    case "5":
+        alert("Es Jueves! =) =)");
+        break;
+    case "6":
+        alert("Es Viernes!! YAY! birra n.n");
+        break;
+    case "7":
+        alert("Es Sabado =)");
+        break;
+    default:
+        alert("Solo son validos dias del 1 al 7");
+        break;
 }
 
 /*
