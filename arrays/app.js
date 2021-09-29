2cfa5675d0615c76fa4050df6c9f55dace7d02b7
jpromanonet <programming@jpromano.net>
2021-09-29 10:39:10 -0300

    Arrays Parte 1

---
+++ arrays/app.js
@@ -0,0 +1,15 @@
+// Como declarar un Array (forma facil)
+
+var autos = ["Saab", "Volvo", "BMW"];
+
+console.log(autos[1]);
+
+// Como declarar un array de forma espaciada, tambien conocida como declaracion multiple
+
+const autos2 = [];
+autos2[0] = "Volvo";
+autos2[1] = "Saab";
+autos2[2] = "BMW";
+
+console.log(autos2[2])
+
--- bucles/app.js
+++ bucles/app.js
@@ -1,4 +1,5 @@
 // Bucle FOR
+
 /*alert("Esto el alert 1");
 alert("Esto el alert 2");
 alert("Esto el alert 3");
@@ -9,16 +10,16 @@ alert("Esto el alert 5");*/
 
 // Usamos VAR
 
-/*
+
 for(var i = 1; i < 6; i++) {
     alert("Esto es el alert " + i);
-}*/
+}
 
 // Usamos LET
 
-/*for(let i = 0; i < 6 ; i++){
+for(let i = 0; i < 6 ; i++){
     alert("Esto es el alert " + i);
-}*/
+}
 
 /*
 variable++ = el simbolo ++ a la derecha, en JS suma un entero a un valor numerico(1)
@@ -28,11 +29,11 @@ variable++ = el simbolo ++ a la derecha, en JS suma un entero a un valor numeric
 let texto = "";
 let i = 0;
 
-/*while(i < 10){
+while(i < 10){
     texto += "El numero es: " + i + "\n";
     alert(texto);
     i++
-}*/
+}
 
 do {
     texto += "El numero es: " + i + "\n";
