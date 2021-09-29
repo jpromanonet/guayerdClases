14becde77bfd4a3b90c611c02158d81735781b79
jpromanonet <programming@jpromano.net>
2021-09-29 11:02:02 -0300

    Arrays Parte 1

--- arrays/app.js
+++ arrays/app.js
@@ -1,8 +1,8 @@
 // Como declarar un Array (forma facil)
 
-var autos = ["Saab", "Volvo", "BMW"];
+const autos = ["Saab", "Volvo", "BMW"];
 
-console.log(autos[1]);
+console.log(autos);
 
 // Como declarar un array de forma espaciada, tambien conocida como declaracion multiple
 
@@ -13,3 +13,44 @@ autos2[2] = "BMW";
 
 console.log(autos2[2])
 
+// The final form para definir un array
+
+const bicicletas = new Array("Mohave","Raleigh","Firebird");
+
+console.log(bicicletas[1]) // Si la posicion no existe es undefined
+
+// Como cambiar los valores de los arrays por posiciones =)
+bicicletas[1] = "KTM";
+
+console.log(bicicletas[1])
+
+bicicletas[3] = "Renault";
+console.log(bicicletas[3]);
+console.log(bicicletas)
+
+// Un array puede contener distintos tipos de datos
+const persona = ["Pedro", "Sin Apellido", 30, true];
+console.log(persona)
+
+// Los arrays pueden contener propiedades internas
+const gato = {
+    nombre: "Remiendo", 
+    edad:"7",
+    color:"Naranja"
+}
+
+console.log(gato)
+console.log(gato.nombre, "es", gato.color)
+
+// ¿Qué mas podemos insertar dentro de un array?
+
+const arrayDeCosas = [];
+
+arrayDeCosas[0] = Date.now;
+arrayDeCosas[1] = gato;
+arrayDeCosas[2] = bicicletas
+arrayDeCosas[3] = autos2[1]
+
+console.log(arrayDeCosas)
+
+// La extensión de un array
