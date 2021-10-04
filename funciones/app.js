bfe709aefe77d75fa7ccf6362ad931af5488f108
jpromanonet <programming@jpromano.net>
2021-10-04 10:29:53 -0300

    Funciones en JS

---
+++ funciones/app.js
@@ -0,0 +1,9 @@
+// Hay 2 formas de declarar una función
+
+// Definiendo una funcion (No se ejecuta inmediatamente)
+function multiplicacion(a, b) {
+    return a* b;
+}
+
+// Declarando una funcion como expresion (se ejecuta inmediatamente)
+const x = function(a,b){return a * b}
