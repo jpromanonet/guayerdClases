1f0090ed60f9019f719c53758e46067f1250f7d6
jpromanonet <programming@jpromano.net>
2021-10-29 12:52:42 -0300

    Clase 41 - Intro

---
+++ 03_JS_Avanzado/DOM/app.js
@@ -0,0 +1,26 @@
+// Como escribir texto desde el DOM
+
+document.getElementById("demo").innerHTML = "Hola Guayerd";
+
+// Como modificar imagenes desde el DOM
+/*document.getElementById("imagenesgato").src = "gato2.png";*/
+
+// Queremos ver la fecha actual
+document.getElementById("fecha").innerHTML = "Fecha: " + Date();
+
+// Podemos escribir en HTML desde JS
+/*document.write("La tarea es hacer un reloj que se vea linda y se actualize solo usando html");*/
+
+// Formularios
+function validateForm(){
+    let x = document.forms["myForm"]["fname"].value;
+    if(x == ""){
+        document.getElementById("validacion").innerHTML = "Es necesario ingresar un nombre";
+        document.getElementById("imagenesgato").src = "gato1.png";
+        return false;
+    } else {
+        document.getElementById("validacion").innerHTML = "Muchas gracias por contactarnos!";
+        document.getElementById("imagenesgato").src = "gato2.png";
+        return false;
+    }
+}
